b16c0f8308c1ef52e351d9d9fd8f81e57944911e
Jayderade <30771579+Jayderade@users.noreply.github.com>
2020-02-11 09:43:12 +1100

    1st Update

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    public float minSpeed;
+    public static float maxSpeed;
+    public float speed;
+    public float jump;
+    public bool jumpOnce = false;
+    public static bool onGround = true;
+    public Rigidbody2D player;
+    public Transform targetPos;
+    public static Rigidbody2D PlayerReference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxSpeed = 10;
+        PlayerReference = player;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Movement
+        if (Input.GetKey(KeyCode.RightArrow) && speed < maxSpeed)
+        {
+            speed = speed + 1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && speed > minSpeed)
+        {
+            speed = speed - 1;
+        }
+        if (onGround)
+        {
+            player.velocity = new Vector2(speed, player.velocity.y);
+            jumpOnce = false;
+        }
+        //Check if grounded
+
+        //if grounded and key pressed 
+        //move up - move towards/ lerp by time or velocity 
+
+        //we are not grounded
+
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            onGround = false;
+            //player = player.velocity.y +jump;
+            if (player.transform.position.y < targetPos.position.y)
+            {
+                //player.transform.position = Vector2.MoveTowards(player.transform.position, targetPos.position, jump * Time.deltaTime);
+                player.velocity = new Vector2(speed, jump);
+            }
+
+            jumpOnce = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpOnce)
+        {
+            //player = player.velocity.y +jump;
+            if (player.transform.position.y < targetPos.position.y)
+            {
+                //player.transform.position = Vector2.MoveTowards(player.transform.position, targetPos.position, jump * Time.deltaTime);
+                player.velocity = new Vector2(speed, jump);
+            }
+            onGround = false;
+            jumpOnce = false;
+        }
+
+        if (player.transform.position.y >= targetPos.position.y)
+        {
+            //player.velocity = new Vector2(speed, -jump);
+
+        }
+
+        #endregion
+
+        
+
+    }
+        
+
+}
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D wave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        wave = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wave.velocity = new Vector2(speed, 0);
+
+    }
+    void DestroyGameObject()
+    {
+        Destroy(Player.PlayerReference);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            DestroyGameObject();
+        }
+    }
+}
